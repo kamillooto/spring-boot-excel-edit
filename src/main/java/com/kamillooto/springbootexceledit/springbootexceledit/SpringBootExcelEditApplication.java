7841f96eea10c5a9f2a02411b45416d02a26c3ee
Kamil Otocki <kamillooto@gmail.com>
2023-02-21 07:22:44 +0100

    First commit

---
+++ src/main/java/com/kamillooto/springbootexceledit/springbootexceledit/SpringBootExcelEditApplication.java
@@ -0,0 +1,13 @@
+package com.kamillooto.springbootexceledit.springbootexceledit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelEditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamillooto/springbootexceledit/springbootexceledit/SpringBootExcelEditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamillooto.springbootexceledit.springbootexceledit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExcelEditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
