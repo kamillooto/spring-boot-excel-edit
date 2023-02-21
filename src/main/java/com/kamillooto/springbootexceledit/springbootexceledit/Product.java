e5a9ce9f8d7903dc4e60773e1d4341b7b7b90a39
Kamil Otocki <kamillooto@gmail.com>
2023-02-21 07:40:58 +0100

    Add Product.java

---
+++ src/main/java/com/kamillooto/springbootexceledit/springbootexceledit/Product.java
@@ -0,0 +1,23 @@
+package com.kamillooto.springbootexceledit.springbootexceledit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private Integer id;
+    private String productName;
+    private Integer price;
+}
